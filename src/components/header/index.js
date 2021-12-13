a7fdaa1a64ce5646c147dcfedfece4444ac720ee
burcu-erkan <burcuerkan.86@gmail.com>
2021-12-13 15:34:31 -0800

    updated

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -11,7 +11,7 @@ const Header = () => {
                 </div>
                 <div className="header-button">
                     <a href="https://github.com/burcu-erkan/expense-app"
-                    target="_blank" rel="noopener no referrer"
+                    target="_blank"
                     ><i class="devicon-github-original"></i>Star</a>
                 </div>
  
